c41f17c6dfb94b93d5d8141ac76ef0e70acd2ac2
sbudiman12 <146686340+sbudiman12@users.noreply.github.com>
2024-12-09 14:47:40 +0700

    debug false

--- mvvm_12/lib/main.dart
+++ mvvm_12/lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.dark(
           primary: Colors.deepPurple, // Main color
